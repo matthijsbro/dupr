04e2afdca420643e5b1d8d10c3e3747f0cdb14a2
whatsnext <matthijsbro@gmail.com>
2025-05-04 23:05:08 +0200

    started switch to tailwind and daisyui
    't was a pain in the a

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     './**/templates/**/*.html', // Scan all Django templates
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   // ---- ADD DAISYUI HERE ----
+//   plugins: [
+//  //   require("daisyui"),
+//   ],
+//   // Optional: Add DaisyUI specific config if needed
+//   //daisyui: {
+//   //  themes: ["light", "dark", "cupcake"], // Example: specify themes you want
+//     // logs: true, // Enable logs - useful for debugging theme issues
+//   // },
+// }
